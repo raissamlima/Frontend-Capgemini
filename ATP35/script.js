b185f43e50e3930be9eedc5deeb261bf305708f3
Raissa M. Lima <raissamartinsarq@gmail.com>
2021-11-04 09:59:20 -0300

    ATP 32, 33, 34 e 35

---
+++ ATP35/script.js
@@ -0,0 +1,12 @@
+function home(){
+    let frame = document.getElementById("frame");
+    frame.src=""
+}
+function contato(){
+    let frame = document.getElementById("frame");
+    frame.src="ContatoHome.html"
+}
+function formacao(){
+    let frame = document.getElementById("frame");
+    frame.src="sistema.html"
+}
